c2f92ac92f5b4ab9273ca734873fa1a94730ef45
unknown <mehedytouhidurzaman@gmail.com>
2023-09-12 23:02:53 +0600

    product container start

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
@@ -10,9 +11,13 @@ const Shop = () => {
     },[])
     return (
         <div className='shop-container'>
-            <div>
-                <h1>card</h1>
-                <p>product count: {products.length}</p>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product
+                    key = {product.id}
+                    product = {product}
+                    ></Product>)
+                }
             </div>
             <div>
                 <h1>sumamry</h1>
